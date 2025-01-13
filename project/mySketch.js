5aa00b7d18b68eb4ae165bcfa66b7bfb5570a27d
hayden-34 <26hayden.allen@lwhs.org>
2025-01-13 23:01:41 +0000

    code

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,17 +1,19 @@
 function setup() {
   createCanvas(400, 400);
-  background(200);
-  translate(200, 200)
-  eyes(0,0)
+  
 }
 
 function draw() {
+  background(200);
+  translate(200, 200);
+  eyes(0,0)
 
 }
+
 function eyes (x, y){
   noStroke()
   //eyelid
-  fill("rgba(173, 148, 57, 0.51)")
+  fill("rgba(147, 147, 147, 0.51)")
   ellipse(x+60, y, 51, 45)
   ellipse(x-60, y, 51, 45)
   //eyes
@@ -19,22 +21,27 @@ function eyes (x, y){
   fill("rgb(213, 213, 213)")
   ellipse(x+60, y, 50, 30);
   ellipse(x-60, y, 50, 30);
+  
   fill("white")
   noStroke()
-  ellipse(x+57, y-2, 44, 20);
-  ellipse(x-63, y-2, 44, 20);
+  ellipse(x+62, y-2, 44, 20);
+  ellipse(x-58, y-2, 44, 20);
+  fill('rgb(239, 123, 177)')
+  ellipse(x-36, 0, 4, 8) 
+  ellipse(x+36, 0, 4, 8)
   //iris 
-  for(let i=10; i>0; i-=1 ){
-    let c = (color(random(0,255), random(0,255), random(0,255)))
-    fill(c)
-    ellipse(x+60, y, i*2, i*2)
+  for(let i=5; i>=0; i-=1 ){
+    let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+  
+    fill(colors[i])
+    ellipse(x+60, y, i*2+15, i*2+15)
+    ellipse(x-60, y, i*2+15, i*2+15)
   }
-
-  for(let i=10; i>0; i-=1 ){
-    let c = (color(random(0,255), random(0,255), random(0,255)))
-    fill(c)
-    ellipse(x-60, y, i*2, i*2)
-  }
-
+  fill('black')
+  ellipse(x+60, y, 10, 10)
+  ellipse(x-60, y, 10, 10)
+  fill('white')
+  ellipse(x+63, y-5, 5, 5)
+  ellipse(x-57, y-5, 5, 5)
 
 }
